0881cd8b6ef2cdfa0ee2cb667fde33b7d52600f2
Steven Yung <stevenyung@icloud.com>
2020-05-29 14:33:06 +0200

    format vue config

--- packages/@tlm/client/vue.config.js
+++ packages/@tlm/client/vue.config.js
@@ -2,12 +2,12 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     proxy: {
-      '^/api': {
-        target: 'http://localhost:9991',
+      "^/api": {
+        target: "http://localhost:9991",
         ws: true,
         changeOrigin: true,
-        pathRewrite: { '^/api': '/' },
-      },
+        pathRewrite: { "^/api": "/" }
+      }
     }
-  },
+  }
 };
