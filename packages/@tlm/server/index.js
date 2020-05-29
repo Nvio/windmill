49e9fa8cb3bb7fc2323052f6288849e51edd2ff0
Steven Yung <stevenyung@icloud.com>
2020-05-29 12:07:15 +0200

    init

---
+++ packages/@tlm/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+
+const app = express()
+const port = 3000
+
+app.get('/api', (req, res) => {
+  const dirPath = path.join(process.cwd(), 'tailwind-lib')
+  const fileNames = fs.readdirSync(dirPath)
+
+  const data = fileNames.map(fileName => {
+    return fs.readFileSync(path.join(dirPath, fileName)).toString()
+  })
+
+  res.send(data)
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
