775699790b94c7fea0a7d25c3658b86be8a5e667
Steven Yung <stevenyung@icloud.com>
2020-05-29 14:59:37 +0200

    add lerna and implement cli package

--- bin/start-tailwind-lib.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-// Run client
-require('../packages/@tlm/client/bin/start')
-
-// Run server
-require('../packages/@tlm/server')
---
+++ packages/@tlm/cli/bin/start-tailwind-lib.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// Run client
+require('../../client/bin/start')
+
+// Run server
+require('../../server')
