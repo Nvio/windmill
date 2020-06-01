fbe3bf53fdcab65badacbd88d197bffa4e371fdb
Steven Yung <stevenyung@icloud.com>
2020-06-01 23:56:34 +0200

    implement filter on .html file only for server scanning dir

--- packages/@tailwind-library-manager/server/index.js
+++ packages/@tailwind-library-manager/server/index.js
@@ -9,11 +9,13 @@ app.get('/', (req, res) => {
   const dirPath = path.join(process.cwd(), 'tailwind-lib')
   const fileNames = fs.readdirSync(dirPath)
 
-  const data = fileNames.map(fileName => {
+  const data = fileNames.filter(fileName => {
+    return !!fileName.match(/\.html$/g);
+  }).map(fileName => {
     return fs.readFileSync(path.join(dirPath, fileName)).toString()
   })
 
   res.send(data)
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
