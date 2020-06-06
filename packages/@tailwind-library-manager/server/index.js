da3ba5ae1598a098c15da21b333410ccd7f5e6bc
Steven Yung <stevenyung@icloud.com>
2020-06-06 14:26:48 +0200

    fix server

--- packages/@tailwind-library-manager/server/index.js
+++ packages/@tailwind-library-manager/server/index.js
@@ -5,17 +5,25 @@ const fs = require('fs')
 const app = express()
 const port = 9991
 
+const dirPath = path.join(process.cwd(), 'tailwind-lib')
+const metadata = require(dirPath);
+
+metadata.components = metadata.components.map(({ name, path: filePath }) => {
+  return {
+    name,
+    content: fs.readFileSync(path.join(dirPath, filePath)).toString(),
+  }
+})
+
 app.get('/', (req, res) => {
-  const dirPath = path.join(process.cwd(), 'tailwind-lib')
-  const fileNames = fs.readdirSync(dirPath)
+  // const fileNames = fs.readdirSync(dirPath)
 
-  const data = fileNames.filter(fileName => {
-    return !!fileName.match(/\.html$/g);
-  }).map(fileName => {
-    return fs.readFileSync(path.join(dirPath, fileName)).toString()
-  })
-
-  res.send(data)
+  // const data = fileNames.filter(fileName => {
+  //   return !!fileName.match(/\.html$/g);
+  // }).map(fileName => {
+  //   return fs.readFileSync(path.join(dirPath, fileName)).toString()
+  // })
+  res.send(metadata)
 })
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
