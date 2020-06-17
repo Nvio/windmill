6b96635b8c818d0a3c9ffc8efcc97508ab5d3b8b
Steven Yung <stevenyung@icloud.com>
2020-06-17 10:24:59 +0200

    rename package to windmill.css

--- packages/@tailwind-library-manager/cli/bin/start-tailwind-lib.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-// Run client
-require('@tailwind-library-manager/client/bin/start')
-
-// Run server
-require('@tailwind-library-manager/server')
---
+++ packages/@windmillcss/cli/bin/start-tailwind-lib.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+// Run client
+require('@windmillcss/client/bin/start')
+
+// Run server
+require('@windmillcss/server')
