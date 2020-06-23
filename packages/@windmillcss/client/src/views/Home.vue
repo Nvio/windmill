bf58cddb2e631081a062028da85209c3dda63e88
Steven Yung <stevenyung@icloud.com>
2020-06-23 23:41:53 +0200

    update home page

--- packages/@windmillcss/client/src/views/Home.vue
+++ packages/@windmillcss/client/src/views/Home.vue
@@ -1,3 +1,5 @@
 <template>
-  <span>Welcome to Windmill!</span>
+  <div class="h-full flex justify-center items-center">
+    <h1 class="text-4xl font-light text-gray-600">Welcome to Windmill!</h1>
+  </div>
 </template>
