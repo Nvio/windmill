5349265b889c0445df1921e7e3e493540911982e
Steven Yung <stevenyung@icloud.com>
2020-07-04 09:29:04 +0200

    init windmill website

---
+++ apps/@windmillcss/website/src/components/SearchInput.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <label for="search" class="sr-only">Search</label>
+    <div class="relative rounded-md shadow-sm">
+      <input
+        id="search"
+        class="form-input block w-full sm:text-sm sm:leading-5"
+        placeholder="Search library or components"
+        autocomplete="off"
+      />
+    </div>
+  </div>
+</template>
