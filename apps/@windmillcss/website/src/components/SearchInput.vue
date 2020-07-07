effa79e5a15603b11291ff4081f20a45017753c1
Steven Yung <stevenyung@icloud.com>
2020-07-07 10:39:32 +0200

    improve styling for header

--- apps/@windmillcss/website/src/components/SearchInput.vue
+++ apps/@windmillcss/website/src/components/SearchInput.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <label for="search" class="sr-only">Search</label>
-    <div class="relative rounded-md shadow-sm">
+    <div class="flex relative">
       <input
         id="search"
-        class="form-input block w-full sm:text-sm sm:leading-5"
+        class="form-input block w-full sm:text-sm sm:leading-5 rounded-r-none border-r-0"
         placeholder="Search library or components"
         autocomplete="off"
       />
+
+      <button class="font-semibold text-white bg-indigo-600 px-8 py-2 rounded-r-md">Search</button>
     </div>
   </div>
 </template>
